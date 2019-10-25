f3dfc45b9d5003adf26a5e3fa7645427c214781c
Xing Wang <coldTea214@gmail.com>
2019-10-25 21:21:46 +0800

    0042

---
+++ 0042/trap.go
@@ -0,0 +1,43 @@
+// i 点的存水量为 min(max(height[:i+1]...), max(height[i:]...)) - height[i]
+
+func trap(height []int) int {
+	if len(height) <= 2 {
+		return 0
+	}
+
+	// maxFromLeft[i] = max{height[:i]}
+	// maxFromRight[i] = max{height[i:]}
+	maxFromLeft := make([]int, len(height))
+	maxFromLeft[0] = height[0]
+	for i := 1; i < len(height); i++ {
+		maxFromLeft[i] = max(height[i], maxFromLeft[i-1])
+	}
+	maxFromRight := make([]int, len(height))
+	maxFromRight[len(height)-1] = height[len(height)-1]
+	for i := len(height) - 2; i >= 0; i-- {
+		maxFromRight[i] = max(height[i], maxFromRight[i+1])
+	}
+
+	var sum int
+	for i := 1; i < len(height)-1; i++ {
+		water := min(maxFromLeft[i-1], maxFromRight[i+1]) - height[i]
+		if water > 0 {
+			sum += water
+		}
+	}
+	return sum
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
