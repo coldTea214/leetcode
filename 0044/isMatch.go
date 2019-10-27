0b7dfb4563e39155f33ac9d9fc249796858baa8c
Xing Wang <coldTea214@gmail.com>
2019-10-27 10:18:48 +0800

    0044

---
+++ 0044/isMatch.go
@@ -0,0 +1,31 @@
+func isMatch(s string, p string) bool {
+	sLen, pLen := len(s), len(p)
+
+	dp := make([][]bool, sLen+1)
+	for i := range dp {
+		dp[i] = make([]bool, pLen+1)
+	}
+	// dp[i][j] == true 表示 s[:i] 和 p[:j] 匹配
+	dp[0][0] = true
+	for j := 1; j <= pLen; j++ {
+		if p[j-1] == '*' {
+			dp[0][j] = dp[0][j-1]
+		}
+	}
+
+	for i := 0; i < sLen; i++ {
+		for j := 0; j < pLen; j++ {
+			if p[j] != '*' {
+				dp[i+1][j+1] = dp[i][j] &&
+					(p[j] == s[i] || p[j] == '?')
+			} else {
+				// p[j] == '*'
+				dp[i+1][j+1] =
+					dp[i][j+1] || // * 匹配任意字符
+						dp[i+1][j] // * 匹配空字符
+			}
+		}
+	}
+
+	return dp[sLen][pLen]
+}
