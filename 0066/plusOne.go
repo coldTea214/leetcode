9ac4dc7f035b1d5f515c27f62149e94d35ca3f13
Xing Wang <coldTea214@gmail.com>
2019-10-29 21:55:19 +0800

    0066-0067

---
+++ 0066/plusOne.go
@@ -0,0 +1,20 @@
+func plusOne(digits []int) []int {
+	if len(digits) == 0 {
+		return []int{1}
+	}
+
+	digits[len(digits)-1]++
+	for i := len(digits) - 1; i > 0; i-- {
+		if digits[i] < 10 {
+			break
+		}
+		digits[i] -= 10
+		digits[i-1]++
+	}
+
+	if digits[0] == 10 {
+		digits = append([]int{1, 0}, digits[1:]...)
+	}
+
+	return digits
+}
---
+++ 0067/addBinary.go
@@ -0,0 +1,44 @@
+// "1", "11"
+func addBinary(a string, b string) string {
+	if len(a) < len(b) {
+		a, b = b, a
+	}
+
+	aInt := toIntSlice(a, len(a))
+	bInt := toIntSlice(b, len(a))
+
+	return makeString(add(aInt, bInt))
+}
+
+// [1,1], [0, 1]
+func toIntSlice(s string, maxLen int) []int {
+	res := make([]int, maxLen)
+	ls := len(s)
+	for i, b := range s {
+		res[maxLen-ls+i] = int(b - '0')
+	}
+	return res
+}
+
+func add(a, b []int) []int {
+	var sum, carry int
+	res := make([]int, len(a))
+	for i := len(a) - 1; i >= 0; i-- {
+		sum = a[i] + b[i] + carry
+		res[i] = sum % 2
+		carry = sum / 2
+	}
+
+	if carry == 1 {
+		return append([]int{1}, res...)
+	}
+	return res
+}
+
+func makeString(nums []int) string {
+	bytes := make([]byte, len(nums))
+	for i := range bytes {
+		bytes[i] = byte(nums[i]) + '0'
+	}
+	return string(bytes)
+}
